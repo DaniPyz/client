6c14ff5a29f44c5caaa2d4d77447718505374939
Dapy <65035781+DaniPyz@users.noreply.github.com>
2022-05-22 22:54:42 +0300

    master

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,12 +3,12 @@ const axiosClient = axios.create({
   baseURL: `https://client-dapy.vercel.app`,
   headers: {
     'Accept': 'application/json',
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/x-www-form-urlencoded',
     'Access-Control-Allow-Origin': '*'
   },
   credentials: 'same-origin',
-  
+
 
 });
-axiosClient.defaults.withCredentials = true
+axiosClient.defaults.withCredentials = false
 export default axiosClient;
